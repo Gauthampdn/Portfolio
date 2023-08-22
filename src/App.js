a3b1e7d0b341604a29a5427257414b8ea462520d
Gautham P <gauthampdn@gmail.com>
2023-08-21 18:31:47 -0700

    edits

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './styles/App.css';
 import Footer from './Footer.js'; // Import the Footer component
 import GridCardLayout from './GridCardLayout';
 import 'font-awesome/css/font-awesome.min.css';
@@ -7,15 +7,16 @@ import 'font-awesome/css/font-awesome.min.css';
 import sideSmileImage from './backgrounds/sidesmile.JPG';
 import serioussmile from './backgrounds/serioussmile.JPG'
 import whiteshadow from './backgrounds/whiteshadow.jpg'
+import food from './backgrounds/food.jpg'
 
 function App() {
   const projects = [
     { 
       title: 'Hi👋 I\'m Gautham Pandian',
-      description: 'I am a dedicated programmer currently a Junior studying Computer Science and Engineering at UC Davis. My solid foundation in computer science, combined with my collaborative spirit, positions me to deliver scalable and efficient solutions. I am always keen on staying up-to-date with the latest technological advancements to ensure I bring the most current and innovative approaches to my work.',
-      theme: 'light',
+      description: 'I am a dedicated programmer currently a Junior studying Computer Science and Engineering at UC Davis. I am always keen on staying up-to-date with the latest technological advancements to ensure I bring the most current and innovative approaches to my work. Below are some of my projects.',
+      theme: 'dark',
       gridClass: 'card-2x1',
-      backgroundImage: whiteshadow,
+      backgroundColor: 'bg-color-black',
       textPosition: 'top-left', // Added this
     },
     { 
@@ -23,11 +24,22 @@ function App() {
       backgroundImage: sideSmileImage,
       textPosition: 'bottom-left', // Added this
     },
+
+    { 
+      title: 'Resume',
+      description: '',
+      theme: 'light',
+      downloadPath: '/files/GPResume.pdf',
+      gridClass: 'card-1x1',
+      backgroundColor: 'bg-color-blue', // or 'path/to/image.jpg' or another color class
+      textPosition: 'top-left', // Added this
+    },
     { 
       title: 'AI Study Planner',
       description: 'An AI study plan creator where students can input a topic and the amount of time they plan to study. The application then generates a detailed study plan for the user based on this input.',
       theme: 'light',
       tags: ['REACT.JS','OPENAI API'],
+      link: 'https://github.com/Gauthampdn/Aggieworkss23',
       gridClass: 'card-1x1',
       backgroundColor: 'bg-color-blue', // or 'path/to/image.jpg' or another color class
       textPosition: 'top-left', // Added this
@@ -37,9 +49,10 @@ function App() {
       title: 'SafeGrub',
       description: 'A web app utilizing computer vision, the Clarifai API, and the Openfoodfacts API to aid visually impaired individuals with food allergies. The app enables users to scan food wrappers using their webcam and extract the serial number. It then parses through a database of ingredients to identify any common allergens associated with the serial number, and uses text-to-speech technology to read the list of ingredients aloud, ensuring accessibility for individuals with visual impairments.',
       tags: ['HTML/CSS', 'JAVASCRIPT', 'REST API'],
+      link: 'https://www.google.com',
       gridClass: 'card-3x1',
       theme: 'dark',
-      backgroundColor: 'bg-color-offwhite',
+      backgroundImage: food,
       textPosition: 'top-left', // Added this
     },
     { 
@@ -51,8 +64,10 @@ function App() {
       textPosition: 'top-left', // Added this
     },
     { 
-      description: 'Description for project 2',
-      gridClass: 'card-1x1',
+      title: 'Pergi',
+      gridClass: 'card-2x2',
+      description: 'I am the Founder of Pergi: Your Ultimate ChatGPT Prompt Assistant. Recognizing the inconvenience of constantly retyping ChatGPT prompts, I embarked on a mission to streamline this process to help with prompt engineering.',
+      tags: ['ASTRO', 'APPSCRIPT', 'EXCEL'],
       backgroundColor: 'bg-color-green',
       textPosition: 'top-left', // Added this
     },
--- src/GridCardLayout.js
+++ src/GridCardLayout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './styles/App.css';
 
 const GridCardLayout = ({ projects }) => {
     return (
@@ -21,6 +21,20 @@ const GridCardLayout = ({ projects }) => {
                     {project.title && <h3>{project.title}</h3>}
                     {project.subtitle && <h4>{project.subtitle}</h4>}
                     {project.description && <p>{project.description}</p>}
+
+                    {/* Conditional rendering of the link button */}
+                    {project.link && (
+                        <button className="button-link" onClick={() => window.open(project.link, '_blank')}>
+                            Visit Link
+                        </button>
+                    )}
+
+                    {/* Conditional rendering of the download button */}
+                    {project.downloadPath && (
+                        <a  className="button-link" href={project.downloadPath} download>
+                            Download
+                        </a>
+                    )}
                 </div>
             ))}
         </div>
